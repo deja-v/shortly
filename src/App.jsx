803f1fd693fe87ea23b58cad7161ca7c93418370
Devang Jain <devangj870@gmail.com>
2024-12-13 19:14:01 +0530

    added input and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
-
-
+import Url from './components/url'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>hi</h1>  
+    <>
+      <h2 className='title'>Shortly</h2>  
+      <Url />
+
+    </>
   )
 }
 
---
+++ src/components/url.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Url(){
+    
+
+    return (
+        <div className="url-paste-container">
+            <h1 className="heading">Shorten Your Loooong Links</h1>
+            <p>Shortly is an efficient and easy-to-use URL shortening service that streamlines your online experience.</p>
+            <div class="wrapper">
+                
+                <input type="text" placeholder="Enter the link" />
+                <button>
+                    Shorten Now!
+                </button>
+            </div>
+        </div>
+    )
+}
